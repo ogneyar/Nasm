806a11e9a4fa8c05afc61e04a578c566bf935067
ogneyar <ogneyar-ne@ya.ru>
2020-12-26 12:45:00 +0300

    hello32 how in viki

--- hello32/hello32.asm
+++ hello32/hello32.asm
@@ -1,14 +1,32 @@
-EXTERN _MessageBoxA@16
-EXTERN _ExitProcess@4
-SECTION .TEXT
-Start:
-	push 0
+; EXTERN _MessageBoxA@16
+; EXTERN _ExitProcess@4
+; SECTION .TEXT
+; Start:
+; 	push 0
+; 	push title
+; 	push banner
+; 	push 0
+; 	call  _MessageBoxA@16
+; 	push 0
+; 	call _ExitProcess@4
+; SECTION .DATA
+; 	banner db 'Hello, world!',0xD,0xA,0
+; 	title db 'Hello',0
+
+%include 'WIN32N.INC'
+EXTERN MessageBoxA
+; Import MessageBoxA user32.dll
+EXTERN ExitProcess
+; Import ExitProcess kernel32.dll
+SECTION CODE USE32 CLASS=CODE
+start:
+	push MB_OK
 	push title
 	push banner
-	push 0
-	call  _MessageBoxA@16
-	push 0
-	call _ExitProcess@4
-SECTION .DATA
+	push NULL
+	call [MessageBoxA]
+	push NULL
+	call [ExitProcess]
+SECTION DATA USE32 CLASS=DATA
 	banner db 'Hello, world!',0xD,0xA,0
 	title db 'Hello',0
