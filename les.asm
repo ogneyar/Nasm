dffeead47b4221e023a1de559aadb4d5cf49580d
ogneyar <ogneyar-ne@ya.ru>
2020-12-28 19:57:01 +0300

    works

--- les.asm
+++ les.asm
@@ -1,10 +1,10 @@
 org 0x100
 
-mov ah, 09h     ; режим вывода
+mov eax, 00000900h  ; 0x0000 + ah == 0x09 al == 0x00 (4 байта, 32 бита)
 mov edx, chegoto
-int 21h         ; вызов прерывания
+int 21h             ; вызов прерывания
 
-mov ax, 4c00h   ; ah == 0x4c al == 0x00
-int 21h         ; выход
+mov eax, 00004c00h  ; 0x0000 + ah == 0x4c al == 0x00
+int 21h             ; выход
 
 chegoto db 0xa,"ABRACADABRA",0xa,'$'
